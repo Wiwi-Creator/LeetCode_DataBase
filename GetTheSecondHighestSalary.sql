c3e5b0e47b676f2fd2f76ca5098e5e5caa3f04f4
Wiwi-Creator <w22151500@kimo.com>
2021-04-23 11:45:13 +0800

    Add files via upload

---
+++ GetTheSecondHighestSalary.sql
@@ -0,0 +1,10 @@
+
+--有些SQL不支援LIMIT (INDEX , COUNT ) 則使用OFFSET
+--抓取從第 INDEX 位置以後的若干(COUNT)筆
+SELECT IFNULL(
+( SELECT   DISTINCT 
+           Salary 
+  FROM     Employee    
+  ORDER BY Salary DESC LIMIT 1 OFFSET 1)
+ ,NULL) AS SecondHighestSalary
+;
