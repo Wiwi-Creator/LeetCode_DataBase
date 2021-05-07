d5b786251c0558ede2ec26292ff92a0e3ba41229
Wiwi_Kuo <w22151500@kimo.com>
2021-05-07 10:04:35 +0800

    Update CombineTwoTables.sql

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -1,6 +1,6 @@
 
-
---SELECT 出題目欄位 建立與address left join(左外部連接) 
+-- MySQL
+-- SELECT 出題目欄位 建立與address left join(左外部連接) 
 SELECT    a.FirstName, a.LastName, b.City, b.State
 FROM      Person a 
 LEFT JOIN Address b
